1a829193a970d14d30536af9b069fc70e054615c
Hasan Farooq <hasan.farooq@tuni.fi>
2020-12-02 19:53:50 +0200

    Code cleanup and calibrations

--- src/application/KukaOnKuka.java
+++ src/application/KukaOnKuka.java
@@ -13,6 +13,8 @@ import Util.Gripper;
 import com.kuka.roboticsAPI.applicationModel.RoboticsAPIApplication;
 import static com.kuka.roboticsAPI.motionModel.BasicMotions.*;
 
+import com.kuka.roboticsAPI.conditionModel.ICondition;
+import com.kuka.roboticsAPI.conditionModel.JointTorqueCondition;
 import com.kuka.roboticsAPI.deviceModel.LBR;
 import com.kuka.roboticsAPI.geometricModel.CartDOF;
 import com.kuka.roboticsAPI.geometricModel.math.Transformation;
@@ -54,93 +56,89 @@ public class KukaOnKuka extends RoboticsAPIApplication {
 	private MediaFlangeIOGroup mediaFlange;
 	
     private static double JOGGING_VELOCITY = 0.1; // moving velocity
-    // Recommended velocity for labyrinth solving: TBD
     
-    private static double CARTESIAN_JERK = 500;
-    // Recommended jerk 50
-
-    private static double FORCE_STOP_THRESHOLD = 4;
+    private static int FORCE_STOP_THRESHOLD = 4;
     
-    private static double MOVEMENT_STEP = 3;
+    private static int DIRECTION_DEBOUNCE_COUNT_THRESHOLD = 3;
+    
+    private int yDirection, directionChangeDebounceCount;
     
 	@Override
 	public void initialize() {
 		// initialize your application here
 		gripper.attachTo(iiwa.getFlange());
 	}
-
-	@Override
-	public void run() {
+	
+	public void initSetup(){
+        double homePositionWithTool[] = new double[] { Math.toRadians(5.17), 
+				Math.toRadians(-51.81),
+				Math.toRadians(-9.72),
+				Math.toRadians(78.27),
+				Math.toRadians(10.14),
+				Math.toRadians(-51.51),
+				Math.toRadians(-6.96) };
+        yDirection = -1;
+        directionChangeDebounceCount = 0;
         mediaFlange.setLEDBlue(false);
-		// your application execution starts here
-        int i = 0;
-        double arr[] = new double[] { Math.toRadians(-17.55), 
-        								Math.toRadians(-57.36),
-        								Math.toRadians(7.7),
-        								Math.toRadians(73.49),
-        								Math.toRadians(-8.85),
-        								Math.toRadians(-50.97),
-        								Math.toRadians(-7.22) };
-        boolean stopRobot = false;
         gripper.attachTo(iiwa.getFlange());
     	gripper.close_M();
-//         move to home position
-//        getLogger().info("Moving to home position");
-//        iiwa.move(ptpHome().setJointVelocityRel(joggingVelocity));
-        getLogger().info("Moving to operation postion");
-        iiwa.move(ptp((Double) Array.get(arr,0),
-        			(Double) Array.get(arr,1),
-        			(Double) Array.get(arr,2),
-        			(Double) Array.get(arr,3),
-        			(Double) Array.get(arr,4),
-        			(Double) Array.get(arr,5),
-        			(Double) Array.get(arr,6)).setJointVelocityRel(JOGGING_VELOCITY));
-//        iiwa.move(ptpHome().setJointVelocityRel(JOGGING_VELOCITY));
+        getLogger().info("Moving to user defined home position");
+        iiwa.moveAsync(linRel().setZOffset(-100).setJointVelocityRel(JOGGING_VELOCITY));
+        iiwa.move(ptp((Double) Array.get(homePositionWithTool,0),
+        			(Double) Array.get(homePositionWithTool,1),
+        			(Double) Array.get(homePositionWithTool,2),
+        			(Double) Array.get(homePositionWithTool,3),
+        			(Double) Array.get(homePositionWithTool,4),
+        			(Double) Array.get(homePositionWithTool,5),
+        			(Double) Array.get(homePositionWithTool,6)).setJointVelocityRel(JOGGING_VELOCITY));
         mediaFlange.setLEDBlue(true);
-        while ( true ){
-
-        	// Open close flange
-//            if(isFlangeOpen){
-//            	gripper.close_M();
-//            	isFlangeOpen = false;
-//            } else {
-//            	gripper.open_M();
-//            	isFlangeOpen = true;
-//            }
-        	while(iiwa.hasActiveMotionCommand()){
-        		getLogger().info("Motion in progress");
-            	try {
-    				TimeUnit.MILLISECONDS.sleep(500);
-    			} catch (InterruptedException e) {
-    				// TODO Auto-generated catch block
-    				e.printStackTrace();
-    			}
-        	}
+	}
+	
+	// Returns true if force is detected during motion above provided threshold
+	public boolean forceControlCheck(int forceThreshold){
+    	while(iiwa.hasActiveMotionCommand()){
         	String logTorque = "";
             ForceSensorData externalData = iiwa.getExternalForceTorque(iiwa.getFlange());
             Vector val = externalData.getForce();
-            int len = (int) val.length();
-            getLogger().info( String.valueOf((int) len) );
-            for(i = 0; i < 3; i++){
+            for(int i = 0; i < 3; i++){
             	logTorque += String.format("%.2f, ", val.get(i));
             }
-            for(i = 0; i < 3; i++){
-            	if(val.get(i) > FORCE_STOP_THRESHOLD || val.get(i) < -FORCE_STOP_THRESHOLD){
-            		stopRobot = true;
+            for(int i = 0; i < 3; i++){
+            	if ((val.get(i) > forceThreshold
+            			|| val.get(i) < -forceThreshold)
+            			&& directionChangeDebounceCount > DIRECTION_DEBOUNCE_COUNT_THRESHOLD){
+                    getLogger().info("External force detected");
+                    iiwa.getController().getExecutionService().cancelAll();
+                	return true;
             	}
             }
+        	directionChangeDebounceCount += 1;
             getLogger().info(logTorque);
-            iiwa.isReadyToMove();
-            if(stopRobot){
-                getLogger().info("External force detected, Stopping!");
-                mediaFlange.setLEDBlue(false);
-                System.exit(0);
-                return;
-            }
-            iiwa.move(linRel().setXOffset(-MOVEMENT_STEP).setJointVelocityRel(JOGGING_VELOCITY).setCartJerk(CARTESIAN_JERK));
-            iiwa.moveAsync(linRel().setYOffset(-MOVEMENT_STEP).setJointVelocityRel(JOGGING_VELOCITY).setCartJerk(CARTESIAN_JERK));
-//            iiwa.moveAsync(linRel().setYOffset(-20));
-            // Y initial -1083.19, Y final -1077.93
+        	try {
+				TimeUnit.MILLISECONDS.sleep(250);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+    	}
+    	return false;
+	}
+	
+	public void moveRobotAsync(boolean changeDirection){
+        if(changeDirection){
+        	changeDirection = false;
+            getLogger().info("Changing direction! yDir=" + yDirection);
+            yDirection *= -1;
+        	directionChangeDebounceCount = 0;
+        }
+        iiwa.moveAsync(linRel().setYOffset(yDirection * 1)); // Watch out the speed if you increase the movement step
+	}
+
+	@Override
+	public void run() {
+		initSetup();
+        while (true) {
+        	boolean changeDirection = forceControlCheck(FORCE_STOP_THRESHOLD);
+        	moveRobotAsync(changeDirection);
         }
 	}
 	
